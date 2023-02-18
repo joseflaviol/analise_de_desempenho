0a9aa1490b3e2061c4eeb9c8e332eb71fb915a73
José Flávio <joseflaviol119@gmail.com>
2023-02-18 00:05:47 -0300

    agabou

--- trabalho_2/etapa2/main.c
+++ trabalho_2/etapa2/main.c
@@ -4,6 +4,13 @@
 #include <math.h>
 #include <time.h>
 
+#define CHEGADA 0
+#define SAIDA 1
+#define COLETA 2
+#define INICIO_CHAMADA 3
+#define ENVIOPACOTE 4
+#define FIM_CHAMADA 6
+
 typedef struct {
     double tempo_final;
 } Chamada;
@@ -11,7 +18,6 @@ typedef struct {
 typedef struct {
     int tipo; // 0 - chegada, 1 - saida, 2 - tempo coleta
     Chamada *chamada; // aponta para uma chamada, caso seja um evento relacionada a ela, aponta para NULL caso contrario
-    bool eh_pacote;
     double tempo; 
 } Evento;
 
@@ -55,6 +61,7 @@ int direita(int i) {
 
 int insere(MinHeap *minHeap, Evento *e) {
     if (minHeap->tamanho == minHeap->capacidade) {
+        printf("-1");
         return -1;
     }
 
@@ -158,11 +165,16 @@ int main(){
 
     FILE *arq_ocupacao;
     FILE *arq_ew;
+    FILE *arq_ew_tempo_real;
     FILE *arq_en;
+    FILE *arq_en_tempo_real;
     FILE *arq_erro_little;
+    FILE *arq_erro_little_tempo_real;
 
     double e_n_final;
+    double e_n_final_temporeal;
     double e_w_final;
+    double e_w_final_temporeal;
     double lambda;
 
     int tempo_coleta;
@@ -191,9 +203,17 @@ int main(){
     little e_w_chegada;
     little e_w_saida;
 
+    little e_n_temporeal;
+    little e_w_chegada_temporeal;
+    little e_w_saida_temporeal;
+
     inicia_little(&e_n);
     inicia_little(&e_w_chegada);
     inicia_little(&e_w_saida);
+
+    inicia_little(&e_n_temporeal);
+    inicia_little(&e_w_chegada_temporeal);
+    inicia_little(&e_w_saida_temporeal);
     /**
     Little -- fim
     */
@@ -223,73 +243,137 @@ int main(){
     // inicia arquivos
     arq_ocupacao = fopen("ocupacao.txt", "w");
     arq_en = fopen("en.txt", "w");
+    arq_en_tempo_real = fopen("en_tempo_real.txt", "w");
     arq_ew = fopen("ew.txt", "w");
+    arq_ew_tempo_real = fopen("ew_tempo_real.txt", "w");
     arq_erro_little = fopen("erro_little.txt", "w");
-    int id = 1;
-    MinHeap *h = inicia_min_heap(1000);
+    arq_erro_little_tempo_real = fopen("erro_little_tempo_real.txt", "w");
 
-    insere(h, novo_evento(2, NULL, 100));
+    MinHeap *h = inicia_min_heap(1000000);
+
+    insere(h, novo_evento(COLETA, NULL, 100));
 
     //gerando o tempo de chegada da primeira requisicao.
     chegada = (-1.0 / (1.0/intervalo_medio_chegada)) * log(aleatorio());
-    insere(h, novo_evento(0, NULL, chegada));
+    insere(h, novo_evento(CHEGADA, NULL, chegada));
     
     // gerando o tempo de inicio da primeira chamada
     chamada = (-1.0 / (1.0/intervalo_medio_chamada)) * log(aleatorio());
-    insere(h, novo_evento(3, NULL, chamada));
+    insere(h, novo_evento(INICIO_CHAMADA, NULL, chamada));
+
+    Evento *e;
 
     while(tempo_decorrido <= tempo_simulacao){
-        Evento *e = extrai_minimo(h);
+        e = extrai_minimo(h);
         tempo_decorrido = e->tempo;
-        //tempo_decorrido = !fila ? minimo(chegada, tempo_coleta) : minimo(tempo_coleta, minimo(chegada, servico));
         
         // inicio chamada
-        if (e->tipo == 3) {
+        if (e->tipo == INICIO_CHAMADA) {
             Chamada *c = (Chamada *) malloc(sizeof(Chamada));
 
             c->tempo_final = tempo_decorrido + (-1.0 / (1.0 / tempo_medio_duracao_chamada)) * log(aleatorio());
             // fim da chamada
-            insere(h, novo_evento( 6, c,  c->tempo_final));
+            insere(h, novo_evento( FIM_CHAMADA, c,  c->tempo_final));
 
             // gera primeiro pacote
-            insere(h, novo_evento( 4, c, tempo_decorrido + 0.02 ));
-        } else if (e->tipo == 4) {
+            insere(h, novo_evento( ENVIOPACOTE, c, tempo_decorrido ));
+
+            // tempo proxima chamada
+            insere(h, novo_evento(INICIO_CHAMADA, NULL, tempo_decorrido + (-1.0 / (1.0/intervalo_medio_chamada)) * log(aleatorio())));
+
+        } else if (e->tipo == ENVIOPACOTE) { // envio do pacote da chamada
 
             // envio do proximo pacote
-            if (e->chamada->tempo_final <= tempo_decorrido + 0.02) {
-                insere(h, novo_evento(4, e->chamada, tempo_decorrido + 0.02));    
+            if (e->chamada->tempo_final >= tempo_decorrido + 0.02) {
+                insere(h, novo_evento(ENVIOPACOTE, e->chamada, tempo_decorrido + 0.02));    
             }
 
             // termino do envio do pacote
-            insere(h, novo_evento( 5, e->chamada, tempo_decorrido + 1280 / largura_link ));
+            insere(h, novo_evento( 5, e->chamada, tempo_decorrido + (160 / largura_link) ));
+            soma_tempo_servico += (160 / largura_link);
+
+            //little
+            e_n.soma_areas += 
+            (tempo_decorrido - e_n.tempo_anterior)*e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos++;
+
+            e_n_temporeal.soma_areas += 
+            (tempo_decorrido - e_n_temporeal.tempo_anterior)*e_n_temporeal.no_eventos;
+            e_n_temporeal.tempo_anterior = tempo_decorrido;
+            e_n_temporeal.no_eventos++;
+
+            e_w_chegada.soma_areas +=
+            (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+            e_w_chegada.tempo_anterior = tempo_decorrido;
+            e_w_chegada.no_eventos++;
+
+            e_w_chegada_temporeal.soma_areas +=
+            (tempo_decorrido - e_w_chegada_temporeal.tempo_anterior) * e_w_chegada_temporeal.no_eventos;
+            e_w_chegada_temporeal.tempo_anterior = tempo_decorrido;
+            e_w_chegada_temporeal.no_eventos++;
 
         } else if (e->tipo == 5) {
 
+            e_n.soma_areas += 
+            (tempo_decorrido - e_n.tempo_anterior)*e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos--;
 
+            e_n_temporeal.soma_areas += 
+            (tempo_decorrido - e_n_temporeal.tempo_anterior)*e_n_temporeal.no_eventos;
+            e_n_temporeal.tempo_anterior = tempo_decorrido;
+            e_n_temporeal.no_eventos--;
 
-        } else if (e->tipo == 2) { // coleta de dados
-            
+            e_w_saida_temporeal.soma_areas +=
+            (tempo_decorrido - e_w_saida_temporeal.tempo_anterior)*e_w_saida_temporeal.no_eventos;
+            e_w_saida_temporeal.tempo_anterior = tempo_decorrido;
+            e_w_saida_temporeal.no_eventos++;
+
+            e_w_saida.soma_areas +=
+            (tempo_decorrido - e_w_saida.tempo_anterior)*e_w_saida.no_eventos;
+            e_w_saida.tempo_anterior = tempo_decorrido;
+            e_w_saida.no_eventos++;
+
+        } else if (e->tipo == FIM_CHAMADA) { // encerra chamada
+            free(e->chamada);  
+        } else if (e->tipo == COLETA) { // coleta de dados
+
             // ocupacao
-            fprintf(arq_ocupacao, "%d %lf\n", tempo_coleta, soma_tempo_servico / maximo(tempo_coleta, servico));
+            fprintf(arq_ocupacao, "%d %lf\n", (int) e->tempo, soma_tempo_servico / maximo(tempo_coleta, servico));
 
             // en
             e_n_final = (e_n.soma_areas + (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos) / tempo_decorrido;
-            fprintf(arq_en, "%d %lf\n", tempo_coleta, e_n_final);
+            fprintf(arq_en, "%d %lf\n", (int) e->tempo, e_n_final);
 
             // ew
             e_w_final = ( (e_w_chegada.soma_areas + (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos) - 
                           (e_w_saida.soma_areas + (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos) ) /
                            e_w_chegada.no_eventos;
-            fprintf(arq_ew, "%d %lf\n", tempo_coleta, e_w_final);
+            fprintf(arq_ew, "%d %lf\n", (int) e->tempo, e_w_final);
 
             // erro de little
             lambda = e_w_chegada.no_eventos / tempo_decorrido;
-            fprintf(arq_erro_little, "%d %.20lf\n", tempo_coleta, fabs(e_n_final - lambda * e_w_final));
+            fprintf(arq_erro_little, "%d %.20lf\n", (int) e->tempo, fabs(e_n_final - lambda * e_w_final));
 
-            insere(h, novo_evento(2, NULL, e->tempo + 100));
+            // en tempo real
+            e_n_final_temporeal = (e_n_temporeal.soma_areas + (tempo_decorrido - e_n_temporeal.tempo_anterior) * e_n_temporeal.no_eventos) / tempo_decorrido;
+            fprintf(arq_en_tempo_real, "%d %lf\n", (int) e->tempo, e_n_final_temporeal);
 
-        } else if (e->tipo == 0) { //chegada
-            //printf("Chegada em %lF.\n", tempo_decorrido);
+            // ew tempo real
+            e_w_final_temporeal = ( (e_w_chegada_temporeal.soma_areas + (tempo_decorrido - e_w_chegada_temporeal.tempo_anterior) * e_w_chegada_temporeal.no_eventos) - 
+                          (e_w_saida_temporeal.soma_areas + (tempo_decorrido - e_w_saida_temporeal.tempo_anterior) * e_w_saida_temporeal.no_eventos) ) /
+                           e_w_chegada_temporeal.no_eventos;
+            fprintf(arq_ew_tempo_real, "%d %lf\n", (int) e->tempo, e_w_final_temporeal);
+
+            // erro de little tempo real
+            lambda = e_w_chegada_temporeal.no_eventos / tempo_decorrido;
+            fprintf(arq_erro_little_tempo_real, "%d %.20lf\n", (int) e->tempo, fabs(e_n_final_temporeal - lambda * e_w_final_temporeal));
+
+            insere(h, novo_evento(COLETA, NULL, e->tempo + 100));
+
+        } else if (e->tipo == CHEGADA) { //chegada
+
             if (!fila) {
                 int t = rand() % 100;
                 servico = tempo_decorrido;
@@ -302,7 +386,7 @@ int main(){
                     servico += (550.0 / largura_link);
                 }
                 
-                insere(h, novo_evento(1, NULL, servico));
+                insere(h, novo_evento(SAIDA, NULL, servico));
 
                 soma_tempo_servico += servico - tempo_decorrido;
             }
@@ -310,7 +394,7 @@ int main(){
             max_fila = fila > max_fila? fila: max_fila;
 
             chegada = tempo_decorrido + (-1.0 / (1.0/intervalo_medio_chegada)) * log(aleatorio());
-            insere(h, novo_evento(0, NULL, chegada));
+            insere(h, novo_evento(CHEGADA, NULL, chegada));
 
             //little
             e_n.soma_areas += 
@@ -324,7 +408,7 @@ int main(){
             e_w_chegada.no_eventos++;
 
         } else {//saida
-            //printf("Saida em %lF.\n", tempo_decorrido);
+
             fila--;
 
             if(fila){
@@ -339,7 +423,7 @@ int main(){
                     servico += (550.0 / largura_link);
                 }
 
-                insere(h, novo_evento(1, NULL, servico));
+                insere(h, novo_evento(SAIDA, NULL, servico));
                 
                 soma_tempo_servico += servico - tempo_decorrido;
             }
@@ -355,6 +439,9 @@ int main(){
             e_w_saida.tempo_anterior = tempo_decorrido;
             e_w_saida.no_eventos++;
         }
+
+        free(e);
+
     }
 
     e_w_chegada.soma_areas +=
@@ -371,11 +458,21 @@ int main(){
     printf("E[N]: %lF\n", e_n_final);
     printf("E[W]: %lF\n", e_w_final);
     printf("lambda: %lF\n\n", lambda);
-
     printf("Erro de Little: %.20lF\n\n", e_n_final - lambda * e_w_final);
 
+    printf("%.2lf %.2lf\n", e_w_chegada.soma_areas, e_w_saida_temporeal.soma_areas);        
+
+    e_n_final_temporeal = e_n_temporeal.soma_areas / tempo_decorrido;
+    e_w_final_temporeal = (e_w_chegada_temporeal.soma_areas - e_w_saida_temporeal.soma_areas) / e_w_chegada_temporeal.no_eventos;
+    lambda = e_w_chegada_temporeal.no_eventos / tempo_decorrido;
+    
+    printf("E[N]: %lF\n", e_n_final_temporeal);
+    printf("E[W]: %lF\n", e_w_final_temporeal);
+    printf("lambda: %lF\n\n", lambda);
+    printf("Erro de Little: %.20lF\n\n", e_n_final_temporeal - lambda * e_w_final_temporeal);
+
     printf("Ocupacao: %lF.\n", soma_tempo_servico/maximo(tempo_decorrido, servico));
-    printf("Max fila: %ld.\n", max_fila);
+
 
     return 0;
 }
